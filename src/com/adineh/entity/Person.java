f234afcb3a5421f067c1a8f6761a3cf7b893f620
epouradineh <e.pouradineh@gmail.com>
2018-10-10 19:04:00 +0330

    no comment

--- src/com/adineh/entity/Person.java
+++ src/com/adineh/entity/Person.java
@@ -1,5 +1,26 @@
 package com.adineh.entity;
 
 abstract public class Person implements Entity{
-
+	private String name;
+	private Integer age;
+	private Gender gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	
 }
